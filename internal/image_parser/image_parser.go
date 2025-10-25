e2032aefc7ea81f024510d1e1fef4caf8e8319dd
Markus Schorer <schorer.markus@outlook.de>
2025-10-25 09:55:58 +0200

    added black white image saver

--- internal/image_parser/image_parser.go
+++ internal/image_parser/image_parser.go
@@ -3,6 +3,8 @@ package image_parser
 import (
 	"fmt"
 	"image"
+	"image/color"
+	"image/jpeg"
 	"os"
 )
 
@@ -21,5 +23,26 @@ func ReadImageFromPath(path string) image.Image {
 		return nil
 	}
 	return img
+
+}
+
+func SaveChunkedImageAsBlackWhite(chunked_brightness [][]int, out_filename string) {
+
+	new_image := image.NewGray(image.Rect(0, 0, len(chunked_brightness[0]), len(chunked_brightness)))
+
+	for y_coord := 0; y_coord < len(chunked_brightness); y_coord++ {
+		for x_coord := 0; x_coord < len(chunked_brightness[0]); x_coord++ {
+			new_image.Set(x_coord, y_coord, color.Gray{uint8(chunked_brightness[y_coord][x_coord])})
+		}
+	}
+	outFile, err := os.Create(out_filename)
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+	error_encode := jpeg.Encode(outFile, new_image, nil)
+	if error_encode != nil {
+		panic(error_encode)
+	}
 
 }
